bab4cd5b994bbbef919ae9147071131a5cda78d3
dbaric99 <damjana.baric1999@gmail.com>
2022-12-21 19:18:35 +0100

    [TER-28] - Create Landing Page

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native';
+import background from '../assets/images/background.jpg';
+import logotype from '../assets/images/logotype.png';
 
 function LandingPage() {
   return (
-    <View>
-        <Text>Landing Page!</Text>
-    </View>
+    <div style = {{backgroundImage: `url(${background})`}}>
+        <img src = {logotype} alt = "" />
+    </div>
   )
 }
 
